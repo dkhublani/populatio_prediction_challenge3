706515b7c5a0fe36956cfeeb777d302fe04b9ff8
drupad <khublani@gmail.com>
2018-09-25 05:06:10 -0500

    coef matrix is now 259*258.

--- main (1).py
+++ main (1).py
@@ -1,41 +1,63 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Sep 21 18:59:32 2018
+Created on Sun Sep 23 12:26:40 2018
 
 Name: khalednakhleh
 """
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import Lasso
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error as mse
 
-def fit(x, y):
+def lasso_reg(alphas, x, y):
     
-    a = 50000000
+    lasso = Lasso(normalize = "True", tol = 0.001, max_iter = 50000)
     
-    coefs = []
-    model = Lasso(tol = 0.02, max_iter = 500000)
-    x = x.T
     w, q = x.shape
-
-    i = 0
-
-    while i < q:
-        t = x.iloc[:,i]
-        x_i = x[x != t]
-        model.set_params(alpha = a)
-        model.fit(x_i, t)
-        coefs.append(model.coef_)
-        i = i + 1
+    f = 1
     
+    error = []
     
-    return coefs
+    for a in alphas:
+        
+        coefs = []
+        preds = []
+        
+        lasso.set_params(alpha = a)
+        i = 0
+       
+        while i < q:
+            
+            
+            population_vector = x.iloc[:, i]
+            train1=x.drop([i],axis=1)
+            lasso.fit(train1, population_vector)
+            test1=y.drop([i],axis=1)
+            prediction = lasso.predict(test1)
+            coefs.append(lasso.coef_)
+            preds.append(prediction)
+            i = i+1
+            
+        name_coef = "coef_no_%s.csv" %f
+        name_preds = "pred_no_%s.csv" %f          
+        
+        np.savetxt(name_coef, coefs, delimiter=",")
+        np.savetxt(name_preds, preds, delimiter=",")
+        
+        y_true = y.values
+        
+        result = mse(y_true, np.transpose(preds))
+        error.append(result)
+        
+        f = f+1  
+    
+    return lasso, preds, error
 
-  
 def main():
+    
+    alphas = 10 ** np.linspace(1,6,5)
     
     population_training_df = pd.read_csv('population_training.csv', encoding='cp1252')
     population_testing_df = pd.read_csv('population_testing.csv', encoding='cp1252')
@@ -44,31 +66,39 @@ def main():
     population_testing_df.drop(['Country Name'], axis=1, inplace=True)
     
     population_training_df = population_training_df.T
-    population_training_df.fillna(population_training_df.mean(),inplace = True)
+    population_training_df.fillna(population_training_df.median(),inplace = True)
     population_training_df = population_training_df.T
     
-    X = population_training_df
-    Y = population_testing_df
-    
+    X = population_training_df.T
+    Y = population_testing_df.T
+
     print(X.shape)
     print(Y.shape)
     
-    coefs = fit(X, Y)
+   # returns prediction and error values of the last iteration
+    model, prediction, error = lasso_reg(alphas, X, Y)
     
-    print(len(coefs))
+    # Graphing for Canada in pred_no_5 file and testing file
+    prediction1=pd.read_csv('pred_no_5.csv', header=None)
+    prediction1=prediction1.T
+    plt.plot(prediction1.iloc[:,33], Y.iloc[:,33])
+    plt.title("True vs. predicted of Canada")
+    plt.xlabel("Predicted values")
+    plt.ylabel("True values" )
+    plt.savefig("canada_pred")
+    plt.show()
     
-    #prediction = predict(Y, coefs)
+    # Graphing the error rate vs. alpha value
+    
+    plt.plot(alphas, error)
+    plt.title("error rate vs. alpha value")
+    plt.xlabel("alpha value")
+    plt.ylabel("mean square error")
+    plt.savefig("alpha_vs_pred")
+    plt.show()
     
 if __name__ == "__main__":
-    
     main()
-
-    
-    
-    
-    
-    
-    
     
     
     
